4949068ff57d6c245cab991e896cb16ab1c31e03
Akshat Sanghvi <jakshat569@gmail.com>
2022-12-06 01:07:17 +0530

    Updated Calculator

--- Calculator.py
+++ Calculator.py
@@ -5,12 +5,14 @@ root = Tk()
 root.tk_setPalette("#E5B8F4")
 root.title("Calculator")
 root.iconbitmap("files/logo.ico")
-
+root.geometry("289x420")
+root.maxsize(289,420)
+root.minsize(289,420)
 e = Entry(root, width=45, borderwidth=8)
 e.grid(row=0,column=0,columnspan=3)
 
 def num(number):
-    ps(f"files/{number%6}.mp3")
+    ps(f"files/{number%6}.mp3",block=False)
     current = e.get()
     e.delete(0,END)
     e.insert(0,f"{current}{number}")
@@ -50,7 +52,7 @@ def divi():
     memory = int(first)
 
 def eq():
-    ps("files/equal.mp3")
+    ps("files/equal.mp3", block=False)
     sec = e.get()
     e.delete(0,END)
     if symbol=="+":
@@ -72,7 +74,7 @@ def dark():
 
 
 def dflt():
-    ps("files/equal.mp3")
+    ps("files/equal.mp3", block=False)
     root.tk_setPalette("#E5B8F4")
     global bcolor
     bcolor = "violet"
@@ -81,25 +83,25 @@ def dflt():
     global fcolor
     fcolor = "black"
 dark()
-ext = Button(root, text="EXIT", padx=32, pady=6, command=exit, fg=fcolor, bg="red")
-num1 = Button(root, text="1", padx=40, pady=20, command=lambda :num(1), fg=fcolor, bg=bcolor)
-num2 = Button(root, text="2", padx=40, pady=20, command=lambda :num(2), fg=fcolor, bg=bcolor)
-num3 = Button(root, text="3", padx=40, pady=20, command=lambda :num(3), fg=fcolor, bg=bcolor)
-num4 = Button(root, text="4", padx=40, pady=20, command=lambda :num(4), fg=fcolor, bg=bcolor)
-num5 = Button(root, text="5", padx=40, pady=20, command=lambda :num(5), fg=fcolor, bg=bcolor)
-num6 = Button(root, text="6", padx=40, pady=20, command=lambda :num(6), fg=fcolor, bg=bcolor)
-num7 = Button(root, text="7", padx=40, pady=20, command=lambda :num(7), fg=fcolor, bg=bcolor)
-num8 = Button(root, text="8", padx=40, pady=20, command=lambda :num(8), fg=fcolor, bg=bcolor)
-num9 = Button(root, text="9", padx=40, pady=20, command=lambda :num(9), fg=fcolor, bg=bcolor)
-num0 = Button(root, text="0", padx=40, pady=20, command=lambda :num(0), fg=fcolor, bg=bcolor)
-add = Button(root, text="+", padx=39, pady=20, command=sum, fg=fcolor, bg=bcolor)
-sub = Button(root, text="-", padx=41, pady=20, command=subtract, fg=fcolor, bg=bcolor)
-mul = Button(root, text="x", padx=40, pady=20, command=mult, fg=fcolor, bg=bcolor)
-div = Button(root, text="/", padx=40, pady=20, command=divi, fg=fcolor, bg=bcolor)
-equal = Button(root, text="=", padx=39, pady=20, command=eq, fg=fcolor, bg=bcolor)
-clear = Button(root, text="Clear", padx=126, pady=6, command=clr, fg=fcolor, bg=clrcolor)
-drk = Button(root, text="Dark Space", padx=16, pady=6, command=dark, fg="white", bg="black")
-de = Button(root, text="Default", padx=22, pady=6, command=dflt, fg="white", bg=bcolor)
+ext = Button(root, text="EXIT", padx=32, pady=6, command=exit, fg=fcolor, bg="red", borderwidth=2)
+num1 = Button(root, text="1", padx=40, pady=20, command=lambda :num(1), fg=fcolor, bg=bcolor, borderwidth=2)
+num2 = Button(root, text="2", padx=40, pady=20, command=lambda :num(2), fg=fcolor, bg=bcolor, borderwidth=2)
+num3 = Button(root, text="3", padx=40, pady=20, command=lambda :num(3), fg=fcolor, bg=bcolor, borderwidth=2)
+num4 = Button(root, text="4", padx=40, pady=20, command=lambda :num(4), fg=fcolor, bg=bcolor, borderwidth=2)
+num5 = Button(root, text="5", padx=40, pady=20, command=lambda :num(5), fg=fcolor, bg=bcolor, borderwidth=2)
+num6 = Button(root, text="6", padx=40, pady=20, command=lambda :num(6), fg=fcolor, bg=bcolor, borderwidth=2)
+num7 = Button(root, text="7", padx=40, pady=20, command=lambda :num(7), fg=fcolor, bg=bcolor, borderwidth=2)
+num8 = Button(root, text="8", padx=40, pady=20, command=lambda :num(8), fg=fcolor, bg=bcolor, borderwidth=2)
+num9 = Button(root, text="9", padx=40, pady=20, command=lambda :num(9), fg=fcolor, bg=bcolor, borderwidth=2)
+num0 = Button(root, text="0", padx=40, pady=20, command=lambda :num(0), fg=fcolor, bg=bcolor, borderwidth=2)
+add = Button(root, text="+", padx=39, pady=20, command=sum, fg=fcolor, bg=bcolor, borderwidth=2)
+sub = Button(root, text="-", padx=41, pady=20, command=subtract, fg=fcolor, bg=bcolor, borderwidth=2)
+mul = Button(root, text="x", padx=40, pady=20, command=mult, fg=fcolor, bg=bcolor, borderwidth=2)
+div = Button(root, text="/", padx=40, pady=20, command=divi, fg=fcolor, bg=bcolor, borderwidth=2)
+equal = Button(root, text="=", padx=39, pady=20, command=eq, fg=fcolor, bg=bcolor, borderwidth=2)
+clear = Button(root, text="Clear", padx=126, pady=6, command=clr, fg=fcolor, bg=clrcolor, borderwidth=2)
+drk = Button(root, text="Dark Space", padx=16, pady=6, command=dark, fg="white", bg="black", borderwidth=2)
+de = Button(root, text="Default", padx=24, pady=6, command=dflt, fg="white", bg=bcolor, borderwidth=2)
 
 ext.grid(row=7,column=2,columnspan=1)
 num0.grid(row=4,column=0)
